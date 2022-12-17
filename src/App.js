4f0ee578f78696ca878229c1a14ebfd72975de36
mdesabd <esa.ce2020@gmail.com>
2022-12-18 01:53:44 +0600

    routes set korlam

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Routes, Route, Link } from "react-router-dom"
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
+import Appointment from './Pages/Appointment/Appointment';
+import Reviews from './Pages/Reviews/Reviews';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="Appointment" element={<Appointment />} />
+        <Route path="Reviews" element={<Reviews />} />
+        <Route path="Contact" element={<Contact />} />
+        <Route path="Login" element={<Login />} />
       </Routes>
     </div>
   );
