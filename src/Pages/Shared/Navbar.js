4a84ff3b3fbd9804f314b82223ebbdd8e3ec50b3
mdesabd <esa.ce2020@gmail.com>
2022-12-18 02:14:57 +0600

    menuItems set kora

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const Navbar = () => {
+    const menuItems = <>
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Appointment</a></li>
+        <li><a>Reviews</a></li>
+        <li><a>Contact Us</a></li>
+        <li><a>Login</a></li>
+    </>
     return (
         <div class="navbar bg-base-100">
             <div class="navbar-start">
@@ -9,24 +17,14 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
-                        <li><a>About</a></li>
-                        <li><a>Appointment</a></li>
-                        <li><a>Reviews</a></li>
-                        <li><a>Contact Us</a></li>
-                        <li><a>Login</a></li>
+                        {menuItems}
                     </ul>
                 </div>
                 <a class="btn btn-ghost normal-case text-xl">Doctors Portal</a>
             </div>
             <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
-                    <li><a>About</a></li>
-                    <li><a>Appointment</a></li>
-                    <li><a>Reviews</a></li>
-                    <li><a>Contact Us</a></li>
-                    <li><a>Login</a></li>
+                {menuItems}
                 </ul>
             </div>
             <div class="navbar-end">
