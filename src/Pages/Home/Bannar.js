49ab40589e9690ca488a907828a820ce503a8f91
mdesabd <esa.ce2020@gmail.com>
2022-12-18 03:13:39 +0600

    bannar change

--- src/Pages/Home/Bannar.js
+++ src/Pages/Home/Bannar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Chair from './../../assets/chair.png'
 
 const Bannar = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen ">
             <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
+                <img src={Chair} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
                     <h1 class="text-5xl font-bold">Box Office News!</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
