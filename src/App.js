3cf36d5f196b6b34db819ddd780ae01e6d1d3e63
mdesabd <esa.ce2020@gmail.com>
2022-12-18 01:33:54 +0600

    routes declare done -4th

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
-import Navbar from './Pages/Home/Shared/Navbar';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
