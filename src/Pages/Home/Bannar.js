cf662afedbf3fbbf49d12f72dab3ddfba83296c3
mdesabd <esa.ce2020@gmail.com>
2022-12-18 04:08:18 +0600

    hero section done

--- src/Pages/Home/Bannar.js
+++ src/Pages/Home/Bannar.js
@@ -4,12 +4,15 @@ import Chair from './../../assets/chair.png'
 const Bannar = () => {
     return (
         <div class="hero min-h-screen ">
-            <div class="hero-content flex-col lg:flex-row-reverse">
+            <div class="hero-content flex-col lg:flex-row-reverse px-14">
                 <img src={Chair} class="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+
+                <div class="text-left">
+                    <h1 class="text-5xl font-bold ">Your New Smile </h1>
+                    <h2 class="text-4xl font-bold ">Starts Here</h2>
+                    <p class="py-6 text-left">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+                    <button class=" btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-cyan-500 to-blue-500">Get Appointment</button>
                 </div>
             </div>
         </div>
