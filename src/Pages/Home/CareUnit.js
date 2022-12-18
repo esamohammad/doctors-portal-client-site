d35f713695b16d19e3bb05f49b610934e0f6bf76
mdesabd <esa.ce2020@gmail.com>
2022-12-18 11:51:02 +0600

    care unit dose 100%

--- src/Pages/Home/CareUnit.js
+++ src/Pages/Home/CareUnit.js
@@ -5,12 +5,11 @@ const CareUnit = () => {
     return (
         <div class="hero min-h-screen px-14">
             <div class="hero-content flex-col lg:flex-row">
-                <img src={Treatment} class="max-w-sm rounded-lg shadow-2xl" />
+                <img src={Treatment} class="w-96 rounded-lg shadow-2xl" />
                 <div className='text-left px-10'>
                     <h1 class="text-5xl font-bold">Exceptionnal Dental Care,</h1>
                     <h1 class="text-5xl font-bold">On Your Terms</h1>
-                    <p class="py-6"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi sit, rem facere fuga saepe inventore deserunt enim repudiandae earum est sequi qui nisi. Maiores maxime earum nam ratione amet qui nesciunt nemo, debitis ex vel quidem accusamus incidunt magni dolore? Optio, ex ut. Culpa ullam consequuntur blanditiis quas deleniti nisi ex unde minima beatae, et incidunt aspernatur fuga architecto modi labore ipsum optio aliquam laudantium iure ab dolorem? Ducimus, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga deleniti quis consequatur excepturi delectus sunt rem eos soluta voluptatibus laborum. Iste doloremque aut quibusdam impedit eius quidem nostrum iure eveniet, culpa facere quo error inventore. Repellat quo deleniti error dignissimos.
-                    </p>
+                    <p class="py-6"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi sit, rem facere fuga saepe inventore deserunt enim repudiandae earum est sequi qui nisi. Maiores maxime earum nam ratione amet qui nesciunt nemo, debitis ex vel quidem accusamus incidunt magni dolore? Optio, ex ut. Culpa ullam consequuntur blanditiis quas deleniti nisi ex unde minima beatae, et incidunt aspernatur fuga architecto modi labore ipsum optio aliquam laudantium iure ab dolorem? Ducimus, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga deleniti quis consequatur excepturi delectus sunt rem eos soluta voluptatibus laborum. Iste doloremque aut quibusdam impedit eius quidem nostrum iure eveniet, culpa facere quo error inventore.Iste doloremque aut quibusdam impedit eius quidem nostrum iure eveniet, culpa facere quo error inventore. Repellat quo deleniti error dignissimos.Iste doloremque aut quibusdam impedit eius quidem nostrum iure eveniet, culpa facere quo error inventore. Repellat quo deleniti error dignissimos. Repellat quo deleniti error dignissimos.                </p>
                     <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-cyan-500 to-blue-500">Learn More</button>
                 </div>
             </div>
