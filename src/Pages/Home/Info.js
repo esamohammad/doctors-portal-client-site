ed7766123b6939f82a05f9ae8e7f54d75ad5f06b
mdesabd <esa.ce2020@gmail.com>
2022-12-18 07:12:47 +0600

    icon set kora hard core style

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import InfoCard from './InfoCard';
 import clock from './../../icons/clock.svg';
+import marker from './../../icons/marker.svg';
+import phone from './../../icons/phone.svg';
+
+
+
+
+
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
+            <InfoCard img={marker}></InfoCard>
+            <InfoCard img={phone}></InfoCard>
         </div>
     );
 };
