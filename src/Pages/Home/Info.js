f177adda8f34c23f07de0b53d0819d340d4ecb8a
mdesabd <esa.ce2020@gmail.com>
2022-12-18 07:24:29 +0600

    bangla system a info te lekha set kora

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -11,9 +11,9 @@ import phone from './../../icons/phone.svg';
 const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={marker}></InfoCard>
-            <InfoCard img={phone}></InfoCard>
+            <InfoCard cardTitle='Opening Hours' img={clock}></InfoCard>
+            <InfoCard cardTitle='Visit Our Location' img={marker}></InfoCard>
+            <InfoCard cardTitle='Contact Us Now' img={phone}></InfoCard>
         </div>
     );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, cardTitle }) => {
     return (
         <div class="card lg:card-side bg-gradient-to-r from-cyan-500 to-blue-500 shadow-xl">
             <figure className='pl-3'>
@@ -8,7 +8,7 @@ const InfoCard = ({ img }) => {
             </figure>
 
             <div class="card-body text-white">
-                <h2 class="card-title">Opening Hours</h2>
+                <h2 class="card-title"> {cardTitle} </h2>
                 <p>Click the button to </p>
 
             </div>
