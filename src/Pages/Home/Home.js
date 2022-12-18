3d1992e5ed9b6843fb16bb78e7782014907b220b
mdesabd <esa.ce2020@gmail.com>
2022-12-18 06:46:36 +0600

    info add kora

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Bannar from './Bannar';
+import Info from './Info';
+
 
 const Home = () => {
     return (
         <div>
             <Bannar></Bannar>
+            <Info></Info>
         </div>
     );
 };
---
+++ src/Pages/Home/Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InfoCard from './InfoCard';
+import clock from './../../icons/clock.svg';
+const Info = () => {
+    return (
+        <div>
+            <InfoCard img={clock}></InfoCard>
+        </div>
+    );
+};
+
+export default Info;
