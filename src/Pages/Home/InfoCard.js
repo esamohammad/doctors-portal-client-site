2e23a3c7b7717dd03d0e4cd7fc4dc20a0f2c931b
mdesabd <esa.ce2020@gmail.com>
2022-12-18 07:04:49 +0600

    padding left

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -3,13 +3,13 @@ import React from 'react';
 const InfoCard = ({ img }) => {
     return (
         <div class="card lg:card-side bg-gradient-to-r from-cyan-500 to-blue-500 shadow-xl">
-            <figure className='m-2'>
+            <figure className='pl-3'>
                 <img src={img} alt="Album" />
             </figure>
 
             <div class="card-body text-white">
                 <h2 class="card-title">Opening Hours</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+                <p>Click the button to </p>
 
             </div>
         </div>
