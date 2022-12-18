e71fdc6a65b97657daab76c4b4f101453f15af0e
mdesabd <esa.ce2020@gmail.com>
2022-12-18 10:31:13 +0600

    our services part done 100% completed

--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Service = ({img}) => {
+const Service = ({ img, name }) => {
     return (
         <div class="card w-96 bg-base-100 shadow-xl">
-  <figure class="px-10 pt-10">
-    <img src={img} alt="Shoes" class="rounded-xl" />
-  </figure>
-  <div class="card-body items-center text-center">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    
-  </div>
-</div>
+            <figure class="px-10 pt-10">
+                <img src={img} alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">{name}</h2>
+                <p>Lorem ipsum dolor sit Lorem ipsum dolor sit amard Lorem ipsum dolor sit amet. </p>
+
+            </div>
+        </div>
     );
 };
 
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -10,9 +10,9 @@ const Services = () => {
             <h3 className='text-3xl font-bold uppercase text-primary py-16'>Our Services</h3>
             <h2 className='text-6xl font-bold py-0'>Service We Provides</h2>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-20 py-20'>
-                <Service img={cavity}></Service>
-                <Service img={fluoride}></Service>
-                <Service img={whitening}></Service>
+                <Service name='Cavity Filling' img={cavity}></Service>
+                <Service name='Fluoride Treatment' img={fluoride}></Service>
+                <Service name='Tooth Whitening' img={whitening}></Service>
             </div>
         </div>
     );
