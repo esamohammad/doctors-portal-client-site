9099bb4969bda3d33cd5567766f53b7a6d88fc09
mdesabd <esa.ce2020@gmail.com>
2022-12-18 12:38:30 +0600

    appointment text is ready

--- src/Pages/Home/Appointment.js
+++ src/Pages/Home/Appointment.js
@@ -9,9 +9,9 @@ const Appointment = () => {
                 <div className='text-left px-10'>
                     <h3 class="text-3xl font-bold uppercase text-primary my-16">Apointment</h3>
                     <h1 class="text-5xl font-bold">Make an Appointment Today</h1>
-                    <p class="py-0"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi sit, rem facere fuga saepe inventore deserunt enim repudiandae earum est sequi qui nisi. Maiores maxime earum nam ratione amet qui nesciunt nemo, debitis ex vel quidem accusamus incidunt magni dolore? Optio, ex ut. Culpa ullam consequuntur blanditiis quas deleniti nisi ex unde minima beatae, et incidunt aspernatur fuga architecto modi labore ipsum optio aliquam laudantium iure ab dolorem? Ducimus, provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga deleniti quis consequatur excepturi delectus sunt rem eos soluta voluptatibus laborum. </p>
+                    <p class="my-8"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi sit, rem facere fuga saepe inventore deserunt enim repudiandae earum est sequi qui nisi. </p>
 
-                    <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-cyan-500 to-blue-500">Learn More</button>
+                    <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-cyan-500 to-blue-500 my-2">Learn More</button>
                 </div>
             </div>
         </div>
