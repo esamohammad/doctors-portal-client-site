5550080708602e22ec87184dce3d19e1d826ac8d
mdesabd <esa.ce2020@gmail.com>
2022-12-18 16:47:31 +0600

    head upore thano, class name a mt-32,40,48,56,... bosiye mt ar age minus diye pai...

--- src/Pages/Home/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment.js
@@ -9,9 +9,9 @@ const MakeAppointment = () => {
                 background: `url(${appointment})`
             }}
         >
-            <div className="hero">
+            <div className="hero mt-56">
                 <div className="hero-content flex-col lg:flex-row">
-                    <img src={doctor} alt="" className="-mt-32 hidden md:block lg:w-1/2 rounded-lg shadow-2xl" />
+                    <img src={doctor} alt="" className="-mt-64 hidden md:block lg:w-1/2 rounded-lg shadow-2xl" />
                     <div className='text-left'>
                         <h4 className='text-lg text-primary font-bold'>Appointment</h4>
                         <h1 className=" text-white text-4xl font-bold">Make an appointment Today</h1>
