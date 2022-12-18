ff2c02a8a3166784b0ef78c511251afbd5ee6c28
mdesabd <esa.ce2020@gmail.com>
2022-12-18 07:01:41 +0600

    text color white set kora

--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -3,14 +3,14 @@ import React from 'react';
 const InfoCard = ({ img }) => {
     return (
         <div class="card lg:card-side bg-gradient-to-r from-cyan-500 to-blue-500 shadow-xl">
-            <figure>
+            <figure className='m-2'>
                 <img src={img} alt="Album" />
             </figure>
 
-            <div class="card-body">
-                <h2 class="card-title">New album is released!</h2>
+            <div class="card-body text-white">
+                <h2 class="card-title">Opening Hours</h2>
                 <p>Click the button to listen on Spotiwhy app.</p>
-               
+
             </div>
         </div>
     );
