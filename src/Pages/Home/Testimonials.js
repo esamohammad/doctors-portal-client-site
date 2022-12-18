ebed31c19e9d9d6e0dba6b7013974ba02f304fdc
mdesabd <esa.ce2020@gmail.com>
2022-12-18 22:05:36 +0600

    testimonial half done

--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -4,18 +4,21 @@ import quote from './../../icons/quote.svg'
 
 const Testimonials = () => {
     return (
-        <section className='py-32 '>
+        <section className='my-28 '>
             <div className='flex justify-between'>
                 <div className='text-left'>
                     <h4 className="text-xl text-primary font-bold"> Testimonials</h4>
-                    <h2 className='text-3xl font-bold py-4'> What Our Patients Say</h2>
+                    <h2 className='text-3xl font-bold my-4'> What Our Patients Say</h2>
                 </div>
                 <div>
                     <img src={quote} className='w-24 lg:w-48' alt='' />
                 </div>
             </div>
 
-            <div></div>
+            <div>
+
+
+            </div>
         </section>
     );
 };
