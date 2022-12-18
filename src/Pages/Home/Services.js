2e0af7830006004de75252b03a4dc292d53e210f
mdesabd <esa.ce2020@gmail.com>
2022-12-18 10:18:36 +0600

    git add for services bangla system a pic add kora

--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Service from './Service';
-import cavity from './../../assets/cavity.png'
+import cavity from './../../assets/cavity.png';
+import fluoride from './../../assets/fluoride.png';
+import whitening from './../../assets/whitening.png';
 
 const Services = () => {
     return (
-        <div>
+        <div >
             <h3 className='text-3xl font-bold uppercase text-primary py-16'>Our Services</h3>
             <h2 className='text-6xl font-bold py-0'>Service We Provides</h2>
-            <Service img={cavity}></Service>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-20 py-20'>
+                <Service img={cavity}></Service>
+                <Service img={fluoride}></Service>
+                <Service img={whitening}></Service>
+            </div>
         </div>
     );
 };
