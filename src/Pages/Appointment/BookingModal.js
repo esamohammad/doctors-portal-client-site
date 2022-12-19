4347de5dcf39c0893e50f7a82917cce8e967f081
mdesabd <esa.ce2020@gmail.com>
2022-12-20 03:49:25 +0600

    modal from ready kora

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -8,9 +8,15 @@ const BookingModal = ({ treatment }) => {
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="font-bold text-lg">Booking for : {name}</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <label htmlFor="booking-modal" className=" btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="font-bold text-lg text-primary">Booking for : {name}</h3>
+
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+
+
+
+                   
                     <div className="modal-action">
                         <label htmlFor="booking-modal" className="btn">Yay!</label>
                     </div>
