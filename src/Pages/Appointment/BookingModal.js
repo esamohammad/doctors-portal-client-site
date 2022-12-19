edc93653fed102f27a6a78e94f16ec389d05820c
mdesabd <esa.ce2020@gmail.com>
2022-12-20 04:09:20 +0600

    form 50%

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -11,24 +11,19 @@ const BookingModal = ({ treatment }) => {
                     <label htmlFor="booking-modal" className=" btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-primary">Booking for : {name}</h3>
 
-                    <form className='grid grid-cols-1 gap-4 justify-items-center'>
+                    <form className='grid grid-cols-1 gap-4 justify-items-center mt-4'>
                         <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        
+
                         <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        
+
                         <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        
+
                         <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        
+
                         <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                        
+
                         <input type="submit" value="Submit" className="btn btn-primary w-full max-w-xs" />
                     </form>
-
-
-                    <div className="modal-action">
-                        <label htmlFor="booking-modal" className="btn">Yay!</label>
-                    </div>
                 </div>
             </div>
         </div>
