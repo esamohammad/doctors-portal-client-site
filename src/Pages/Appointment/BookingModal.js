2bafb101a0b777c703524361eef53932972e3a49
mdesabd <esa.ce2020@gmail.com>
2022-12-20 04:04:48 +0600

    justify-items-center ,modal form ready kora

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -11,8 +11,18 @@ const BookingModal = ({ treatment }) => {
                     <label htmlFor="booking-modal" className=" btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-primary">Booking for : {name}</h3>
 
-                    <form >
+                    <form className='grid grid-cols-1 gap-4 justify-items-center'>
                         <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        
+                        <input type="submit" value="Submit" className="btn btn-primary w-full max-w-xs" />
                     </form>
 
 
