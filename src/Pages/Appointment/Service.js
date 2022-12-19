7a2d9270231f97c92d230224f10668a2a4ab9fc3
mdesabd <esa.ce2020@gmail.com>
2022-12-20 00:43:34 +0600

    time slot dhekhano

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -6,10 +6,15 @@ const Service = ({ service }) => {
         <div className="card lg: file:max-w-lg bg-base-100 shadow-2xl">
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p className='text-xl'>{slots.length} {slots.length>1 ? 'spaces':'space'} available</p>
+
+                <p>{slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-600'>No Slot Available</span>}</p>
+
+                <p className='text-xl'>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Buy Now</button>
                 </div>
+
             </div>
         </div>
     );
