3a5f16a26c6fd5a76b4fee317d260ea241f45e34
mdesabd <esa.ce2020@gmail.com>
2022-12-19 23:38:30 +0600

    sercice card add for availabale appointment

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({ service }) => {
     return (
-        <div>
-            
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
