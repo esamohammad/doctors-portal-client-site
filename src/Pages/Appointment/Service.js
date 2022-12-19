0d6d37fbc2d40bd75a88e50a6e223bcbe9291f8b
mdesabd <esa.ce2020@gmail.com>
2022-12-20 01:08:53 +0600

    color set kora ,justify center etc

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,15 +4,16 @@ const Service = ({ service }) => {
     const { name, slots } = service;
     return (
         <div className="card lg: file:max-w-lg bg-base-100 shadow-2xl">
-            <div className="card-body">
-                <h2 className="card-title">{name}</h2>
+            <div className="card-body ">
+               
+                <div><h2 className="card-title justify-center py-4 text-primary">{name}</h2>
 
                 <p>{slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-600'>No Slot Available</span>}</p>
 
-                <p className='text-xl'>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <p className='text-xl'>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p></div>
 
-                <div className="card-actions justify-end">
-                    <button disabled={slots.length === 0} className="btn btn-primary">Buy Now</button>
+                <div className="card-actions justify-center">
+                    <button disabled={slots.length === 0} className="btn btn-primary text-white ">Book Appointment</button>
                 </div>
 
             </div>
