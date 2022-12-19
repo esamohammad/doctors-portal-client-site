c9d9476e13a409a8528a199c767cab4d95ffb3e8
mdesabd <esa.ce2020@gmail.com>
2022-12-20 00:19:45 +0600

    slots length

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -6,7 +6,7 @@ const Service = ({ service }) => {
         <div className="card lg: file:max-w-lg bg-base-100 shadow-2xl">
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p className='text-xl'>{slots.length} spaces available</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Buy Now</button>
                 </div>
