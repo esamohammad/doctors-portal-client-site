d89c10b27ff9eb9c833b46f9bada33dd3b12fcce
mdesabd <esa.ce2020@gmail.com>
2022-12-20 03:53:16 +0600

    modal a form

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -11,12 +11,11 @@ const BookingModal = ({ treatment }) => {
                     <label htmlFor="booking-modal" className=" btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg text-primary">Booking for : {name}</h3>
 
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <form >
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    </form>
 
 
-
-
-                   
                     <div className="modal-action">
                         <label htmlFor="booking-modal" className="btn">Yay!</label>
                     </div>
