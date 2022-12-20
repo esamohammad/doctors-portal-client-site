f630c0f2ffdf8a0a49e222a8f70f36e119f68cb8
mdesabd <esa.ce2020@gmail.com>
2022-12-21 01:46:00 +0600

    text center

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,11 +6,9 @@ const Login = () => {
         <div className='flex h-screen justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
+                <h2 className="text-center text-2xl font-bold">Login</h2>
+                    
+                    
                 </div>
             </div>
         </div>
