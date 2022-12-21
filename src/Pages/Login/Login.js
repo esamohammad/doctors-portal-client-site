39c27f9804274ae4e9009d4e14717111e83bb202
mdesabd <esa.ce2020@gmail.com>
2022-12-21 06:39:38 +0600

    value send kora

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,20 +1,32 @@
 
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init'
 import { useForm } from "react-hook-form";
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm()
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
 
 
-    if (user) {
-        console.log(user);
+
+
+
+
+
+    if (gUser) {
+        console.log(gUser);
     }
 
     const onSubmit = data => {
         console.log(data);
+        signInWithEmailAndPassword(data.email, data.password);
     }
 
     return (
@@ -84,7 +96,7 @@ const Login = () => {
                     </form>
 
                     <div className="divider">OR</div>
-                    
+
                     <button
                         onClick={() => signInWithGoogle()}
                         className="btn btn-outline"
