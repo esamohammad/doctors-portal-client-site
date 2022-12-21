e9191f6942da35a4d75acbd51eee2ece38cf2209
mdesabd <esa.ce2020@gmail.com>
2022-12-22 00:03:44 +0600

    loading spinar add kora;

--- src/Pages/Shared/Loding.js
+++ src/Pages/Shared/Loding.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import loding from './../../icons/loding.gif'
 const Loading = () => {
     return (
         <div className="flex items-center justify-center ">
-            <div className="w-16 h-16 border-b-2 border-primary rounded-full animate-spin"></div>
+            <div>
+                <img src={loding} className="max-h-60  rounded-lg " alt='loading spinner' />
+
+            </div>
         </div>
     );
 };
 
 export default Loading;
+
