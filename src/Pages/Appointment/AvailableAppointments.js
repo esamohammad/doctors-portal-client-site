6a6f56d613be5f45376496fc28d87eb7ca83e89f
mdesabd <esa.ce2020@gmail.com>
2022-12-22 21:42:23 +0600

    react quary update work halp kaj

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,18 +1,31 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loding';
 import BookingModal from './BookingModal';
 import Service from './Service';
 
 const AvailableAppointments = ({ date }) => {
-    const [services, setServices] = useState([]);
+    // const [services, setServices] = useState([]); R.Q
     const [treatment, setTreatment] = useState(null);
 
     const formattedDate = format(date, 'PP');
-    useEffect(() => {
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/available?date=${formattedDate}`)
+    //         .then(res => res.json())
+    //         .then(data => setServices(data));
+    // }, [formattedDate]);  R.Q
+
+    const { data: services, isLoading } = useQuery('available', () =>
         fetch(`http://localhost:5000/available?date=${formattedDate}`)
             .then(res => res.json())
-            .then(data => setServices(data));
-    }, [formattedDate]);
+    )
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+
 
     return (
         <div>
@@ -20,7 +33,7 @@ const AvailableAppointments = ({ date }) => {
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    services.map(service => <Service
+                    services?.map(service => <Service
                         key={service._id}
                         service={service}
                         setTreatment={setTreatment}
