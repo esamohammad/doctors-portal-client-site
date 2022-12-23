960c6b1b80d1e2d72a21e8a725cdd890aa535142
mdesabd <esa.ce2020@gmail.com>
2022-12-24 05:38:47 +0600

    upsert ar kaj ok akhon jwt token suru korbo.

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -43,7 +43,7 @@ const SignUp = () => {
 
 
 
-    if (user || gUser) {
+    if (token) {
         navigate('/Appointment')
 
     }
--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -3,9 +3,52 @@ import { useEffect, useState } from "react";
 const useToken = user => {
     const [token, setToken] = useState('');
     useEffect(() => {
-        console.log('user inside useToken', user);
+        const email = user?.user?.email;
+        const currentUser = { email:email };
+        if (email) {
+            fetch(`http://localhost:5000/user/${email}`, {
+
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(currentUser)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log('data inside useToken', data);
+                })
+        }
+
     }, [user]);
     return [token];
 }
 
 export default useToken;
+
+
+// import { useEffect, useState } from "react";
+
+// const useToken = user => {
+//     const [token, setToken] = useState('');
+//     useEffect(() => {
+//         console.log('user inside useToken', user);
+//     }, [user]);
+//     return [token];
+// }
+
+// export default useToken;
+
+
+/*
+// ===================================
+upsert ar kaj ok akhon jwt token suru korbo.
+//====================================
+*/
+
+
+
+
+
+
+
