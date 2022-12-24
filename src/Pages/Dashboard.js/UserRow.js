f0de5f6e39a9a372f59a4d039ab000a41e4826df
mdesabd <esa.ce2020@gmail.com>
2022-12-24 19:53:33 +0600

    {/* email niye distucturing */}

--- src/Pages/Dashboard.js/UserRow.js
+++ src/Pages/Dashboard.js/UserRow.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const UserRow = ({user}) => {
+    const {email}=user;
     return (
         <tr>
             <th>1</th>
-            <td>Cy Ganderton</td>
+            <td>{email}</td> 
+            {/* email niye distucturing */}
             <td>Quality Control Specialist</td>
             <td>Blue</td>
         </tr>
