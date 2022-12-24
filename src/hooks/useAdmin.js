5ff6df7e7c81a9b741d93830c5e4800c88c7bdb8
mdesabd <esa.ce2020@gmail.com>
2022-12-25 00:44:39 +0600

    code ai porjonto nijeke admin kora

---
+++ src/hooks/useAdmin.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react"
+
+const useAdmin = user => {
+    const [admin, setAdmin] = useState(false);
+    const [adminLoading, setAdminLoading] = useState(true);
+    useEffect( () =>{
+        const email = user?.email;
+        if(email){
+            fetch(`http://localhost:5000/admin/${email}`, {
+                method:'GET',
+                headers: {
+                    'content-type': 'application/json',
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+            .then(res=>res.json())
+            .then(data => {
+                setAdmin(data.admin);
+                setAdminLoading(false);
+            })
+        }
+    }, [user])
+
+    return [admin, adminLoading]
+}
+
+export default useAdmin;
+
+//ai porjonto holo nijeke nije admin ghoshonar code ar pore kaj holo ai add min ke requre auth kore felbo ar pore ai hhoks lagbe ..useAdmin use korbe.
