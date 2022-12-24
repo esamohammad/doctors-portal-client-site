619cfeb862bc2d1befacb5c1c4ec75dd01f17867
mdesabd <esa.ce2020@gmail.com>
2022-12-24 07:03:21 +0600

    jwt token ar kaj

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -4,7 +4,7 @@ const useToken = user => {
     const [token, setToken] = useState('');
     useEffect(() => {
         const email = user?.user?.email;
-        const currentUser = { email:email };
+        const currentUser = { email: email };
         if (email) {
             fetch(`http://localhost:5000/user/${email}`, {
 
@@ -17,6 +17,10 @@ const useToken = user => {
                 .then(res => res.json())
                 .then(data => {
                     console.log('data inside useToken', data);
+                    const accessToken = data.token;
+                    localStorage.setItem('accessToken', accessToken);
+                    setToken(accessToken);
+
                 })
         }
 
