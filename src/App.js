ccf6ac8a4858e1b3eaa9710baa5fae0c2e5f214e
mdesabd <esa.ce2020@gmail.com>
2022-12-24 10:41:56 +0600

    new index route my user

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Dashboard from './Pages/Dashboard.js/Dashboard';
 import MyAppointment from './Pages/Dashboard.js/MyAppointment';
 import MyReview from './Pages/Dashboard.js/MyReview';
 import MyHistory from './Pages/Dashboard.js/MyHistory';
+import MyUser from './Pages/Dashboard.js/MyUser';
 
 function App() {
   return (
@@ -42,6 +43,7 @@ function App() {
           <Route index element={<MyAppointment></MyAppointment>} />
           <Route path="review" element={<MyReview></MyReview>} />
           <Route path="history" element={<MyHistory></MyHistory>} />
+          <Route path="user" element={<MyUser></MyUser>} />
           {/* ***index routing */}
         </Route>
 
---
+++ src/Pages/Dashboard.js/MyUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyUser = () => {
+    return (
+        <div className='justify-center'>
+            <h2 className='text-center text-blue-700 text-2xl'>My User</h2>
+        </div>
+    );
+};
+
+export default MyUser;
