d9499afd7817eeba3c6230111245dc8a194dd4ca
mdesabd <esa.ce2020@gmail.com>
2022-12-24 17:40:37 +0600

    loading is sommossa at dur korar jonno loding

--- src/Pages/Dashboard.js/Users.js
+++ src/Pages/Dashboard.js/Users.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import Loading from '../Shared/Loding';
 
 
 const Users = () => {
     //react quary
-    const { data:users } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+    const { data:users,isLoading } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+
+    //(is loding , loding sommossa)
+    if (isLoading) {
+        return <Loading></Loading>
+        
     return (
         <div className='justify-center'>
             <h2 className='text-center text-blue-700 text-2xl'>All User: {users.lenth} </h2>
