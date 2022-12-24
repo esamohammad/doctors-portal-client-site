44aaa10e6b563f344b83113468028d9fe61850e4
mdesabd <esa.ce2020@gmail.com>
2022-12-24 19:22:39 +0600

    data dekhanor jonno table neya

--- src/Pages/Dashboard.js/Users.js
+++ src/Pages/Dashboard.js/Users.js
@@ -14,6 +14,30 @@ const Users = () => {
     return (
         <div className='justify-center'>
             <h2 className='text-center text-blue-700 text-2xl'>All User: {users.length} </h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        {/* atai repet korbo */}
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
