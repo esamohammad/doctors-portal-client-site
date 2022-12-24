32953c48316f6f59da13756c3ea0fbb1ff7ed4fb
mdesabd <esa.ce2020@gmail.com>
2022-12-24 20:01:08 +0600

    admin button add

--- src/Pages/Dashboard.js/UserRow.js
+++ src/Pages/Dashboard.js/UserRow.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const UserRow = ({user}) => {
-    const {email}=user;
+const UserRow = ({ user }) => {
+    const { email } = user;
     return (
         <tr>
             <th>1</th>
-            <td>{email}</td> 
+            <td>{email}</td>
             {/* email niye distucturing */}
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+            <td><button class="btn btn-xs">Make Admin</button></td>
+            <td><button class="btn btn-xs">Remove User</button></td>
+            {/* Remove user ata amake korte hobe pore */}
         </tr>
     );
 };
