6613a1a60a9c657d87955d5c2e80d17e301571a3
mdesabd <esa.ce2020@gmail.com>
2022-12-28 05:07:36 +0600

    chobi database a gese

--- src/Pages/Dashboard.js/AddDoctor.js
+++ src/Pages/Dashboard.js/AddDoctor.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
 import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 import Loading from '../Shared/Loading';
 
 const AddDoctor = () => {
-    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit,reset } = useForm();
 
     const { data: services, isLoading } = useQuery('services', () => fetch('http://localhost:5000/service').then(res => res.json()))
 
@@ -32,10 +33,33 @@ const AddDoctor = () => {
         })
             .then(res => res.json())
             .then(result => {
-                console.log('imgbb', result)
 
+                if (result.success) {
+                    const img = result.data.url;
+                    const doctor = {
+                        name: data.name,
+                        email: data.email,
+                        specialty: data.specialty,
+                        img: img
+                    }
+                    // send to your database 
+                    fetch('http://localhost:5000/doctor', {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                        },
+                        body: JSON.stringify(doctor)
+                    })
+                    .then(res =>res.json())
+                    .then(inserted =>{
+                        console.log('doctor',inserted)
+                    })
+    
+                }
+                
             })
-    }
+        }
 
     if (isLoading) {
         return <Loading></Loading>
