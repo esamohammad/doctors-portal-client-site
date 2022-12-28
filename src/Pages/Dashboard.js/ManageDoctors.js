9c513fcf7e653f6b020f580a10e53acb77c6972c
mdesabd <esa.ce2020@gmail.com>
2022-12-28 22:41:13 +0600

    delete done with tost

--- src/Pages/Dashboard.js/ManageDoctors.js
+++ src/Pages/Dashboard.js/ManageDoctors.js
@@ -5,7 +5,7 @@ import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
 
-    const { data: doctors, isLoading } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
+    const { data: doctors, isLoading, refetch } = useQuery('doctors', () => fetch('http://localhost:5000/doctor', {
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
         }
@@ -35,6 +35,7 @@ const ManageDoctors = () => {
                                 key={doctor._key}
                                 doctor={doctor}
                                 index={index}
+                                refetch={refetch}
                                 ></DoctorRow>)
                         }
                     </tbody>
